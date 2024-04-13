f5725f1eba7493fc71fc10ddbc6e0ce27b18112c
sawan4h5j <154305330+sawan4h5j@users.noreply.github.com>
2024-04-13 16:42:26 +0600

    about section edeted

--- src/page/AboutUs/AboutUs.jsx
+++ src/page/AboutUs/AboutUs.jsx
@@ -1,11 +1,50 @@
 const AboutUs = () => {
   return (
-    <div className="card card-body card-compact caret-red-700 bg-slate-700 text-2xl leading-relaxed text-center">
-      <h3 className="font-bold text-4xl mb-2">About Our Debating Club</h3>
-      <p>
-        Welcome to DAGMC-Club, where minds collide, ideas flourish, and
-        arguments shape the future. 
-      </p>
+    <div>
+      <div className="card card-body card-compact caret-red-700  text-2xl leading-relaxed text-center  max-w-80 md:max-w-full mx-auto w-full p-3  font-bold bg-[#00f7ff] shadow-2xl shadow-[#00f7ff] relative border-2 rounded-lg bg-transparent py-2.5 px-5  transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full text-[#00f7ff] before:w-full before:origin-top-left before:scale-y-0 before:bg-[#00f7ff] before:transition-transform border-[#00f7ff]">
+        <h3 className="font-bold text-red-500 text-4xl mb-2">
+          About Our Debating Club
+        </h3>
+        <p>
+          Welcome to our debate club, where ideas collide and minds ignite! We
+          are a passionate community dedicated to honing critical thinking
+          skills, fostering respectful dialogue, and embracing diverse
+          perspectives. Through spirited discourse, we cultivate empathy,
+          understanding, and intellectual growth. Our members span a spectrum of
+          backgrounds, united by a shared love for engaging in lively debates on
+          topics ranging from politics and philosophy to science and culture.
+          Whether you're a seasoned debater or just beginning your journey, our
+          club provides a welcoming space to challenge assumptions, sharpen
+          rhetoric, and forge lasting connections. Join us in shaping the
+          future, one argument at a time!
+        </p>
+      </div>
+      <div className="card card-body card-compact caret-red-700  text-2xl leading-relaxed text-center  max-w-80 md:max-w-full mx-auto w-96 p-3  font-bold bg-[#00f7ff] shadow-2xl shadow-[#00f7ff] relative border-2 rounded-lg bg-transparent py-2.5 px-5  transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full text-[#00f7ff] before:w-full before:origin-top-left before:scale-y-0 before:bg-[#00f7ff] before:transition-transform border-[#00f7ff] mt-8 mr-48">
+        <h3 className="font-bold text-red-500 text-4xl mb-2">
+          About Our Debating Club
+        </h3>
+        <p>
+         
+          Whether you're a seasoned debater or just beginning your journey, our
+          club provides a welcoming space to challenge assumptions, sharpen
+          rhetoric, and forge lasting connections. Join us in shaping the
+          future, one argument at a time!
+        </p>
+      </div>
+
+      <div className="card card-body card-compact caret-red-700  text-2xl leading-relaxed text-center  max-w-80 md:max-w-full mx-auto w-96 p-3  font-bold bg-[#00f7ff] shadow-2xl shadow-[#00f7ff] relative border-2 rounded-lg bg-transparent py-2.5 px-5  transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full text-[#00f7ff] before:w-full before:origin-top-left before:scale-y-0 before:bg-[#00f7ff] before:transition-transform border-[#00f7ff] ml-32 mb-96">
+        <h3 className="font-bold text-red-500 text-4xl mb-2">
+          About Our Debating Club
+        </h3>
+        <p>
+         
+          Whether you're a seasoned debater or just beginning your journey, our
+          club provides a welcoming space to challenge assumptions, sharpen
+          rhetoric, and forge lasting connections. Join us in shaping the
+          future, one argument at a time!
+        </p>
+      </div>
+     
     </div>
   );
 };
