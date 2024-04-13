a1d295cfdefca928519f455d5b62c916c342c2c0
sawan4h5j <154305330+sawan4h5j@users.noreply.github.com>
2024-04-13 16:02:46 +0600

    about p tag change by shawon

--- src/page/AboutUs/AboutUs.jsx
+++ src/page/AboutUs/AboutUs.jsx
@@ -1,22 +1,13 @@
-
-
 const AboutUs = () => {
-    return (
-        <div className="card card-body card-compact caret-red-700 bg-slate-700 text-2xl leading-relaxed text-center">
-            <h3 className="font-bold text-4xl mb-2">About Our Debating Club</h3>
-            <p>
-
-Welcome to DAGMC-Club, where minds collide, ideas flourish, and arguments shape the future. We are a vibrant community dedicated to fostering critical thinking, effective communication, and intellectual growth through the art of debate.
-
-Established with the mission of providing a platform for students to engage in thoughtful discourse and develop essential life skills, our club offers a dynamic space where members can explore diverse perspectives, challenge assumptions, and refine their ability to articulate ideas with clarity and conviction.
-
-At DAGMC-Club, we believe in the power of dialogue to inspire change and drive progress. Whether you're a seasoned debater or new to the world of rhetoric, our inclusive environment welcomes individuals of all backgrounds and experiences. From parliamentary debates to impromptu speaking sessions, our events cater to a wide range of interests and skill levels, ensuring that every member has the opportunity to participate and grow.
-
-Beyond honing speaking and argumentation skills, our club provides valuable opportunities for personal and professional development. Through workshops, guest lectures, and competitive tournaments, members gain confidence, expand their knowledge base, and build lasting connections with peers who share their passion for intellectual inquiry.
-
-Join us at DAGMC-Club and embark on a journey of self-discovery, intellectual exploration, and meaningful engagement with the world around you. Together, let's elevate discourse, amplify voices, and champion the values of reason, empathy, and open-mindedness.</p>
-        </div>
-    );
+  return (
+    <div className="card card-body card-compact caret-red-700 bg-slate-700 text-2xl leading-relaxed text-center">
+      <h3 className="font-bold text-4xl mb-2">About Our Debating Club</h3>
+      <p>
+        Welcome to DAGMC-Club, where minds collide, ideas flourish, and
+        arguments shape the future. 
+      </p>
+    </div>
+  );
 };
 
 export default AboutUs;
